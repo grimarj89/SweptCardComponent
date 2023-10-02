8e414c49d03ce6c62c2dff6c94477de5858b06a3
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2023-10-02 15:30:47 -0600

    added a circular indicator

--- app/src/main/java/com/grimgdl/stats/ui/Person.kt
+++ app/src/main/java/com/grimgdl/stats/ui/Person.kt
@@ -4,15 +4,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
@@ -21,7 +20,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 
 @Composable
 fun PersonCard(
@@ -37,12 +36,21 @@ fun PersonCard(
             modifier = modifier
         ){
 
-            AsyncImage(
+
+            SubcomposeAsyncImage(
                 model = img,
-                contentDescription = null,
+                contentDescription =null,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
+                loading = {
+
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(32.dp)
+                    )
+
+                }
             )
+
 
             Column(
                 modifier = Modifier
