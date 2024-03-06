c00b704244e2bdc3f9cbd811e01580419192f230
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2024-03-05 18:36:32 -0600

    images indicator

--- app/src/main/java/com/grimgdl/stats/ui/Person.kt
+++ app/src/main/java/com/grimgdl/stats/ui/Person.kt
@@ -1,15 +1,23 @@
 package com.grimgdl.stats.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -18,6 +26,7 @@ import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
@@ -30,29 +39,58 @@ fun PersonCard(
     description: String
 ) {
 
+    val photoList = remember { mutableStateOf(mutableListOf(randomImage(), randomImage(), randomImage(), randomImage())) }
+
+
     Card {
 
         Box(
             modifier = modifier
-        ){
+        ) {
 
 
             SubcomposeAsyncImage(
                 model = img,
-                contentDescription =null,
+                contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize(),
                 loading = {
-                    Box (
+                    Box(
                         modifier = Modifier.size(32.dp),
                         contentAlignment = Alignment.Center
-                    ){
+                    ) {
 
                         CircularProgressIndicator()
                     }
 
                 }
             )
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+
+
+                photoList.value.forEachIndexed { index, _ ->
+
+                    Box(
+                        modifier = Modifier
+                            .height(5.dp)
+                            .weight(1f)
+                            .background(color = Color.White)
+
+                    )
+
+                    if (index < photoList.value.size - 1) {
+                        Spacer(modifier = Modifier.width(5.dp))
+                    }
+
+                }
+
+            }
 
 
             Column(
@@ -127,6 +165,12 @@ fun randomImage(): String {
 data class Person(
     val id: Int,
     val name: String,
-    val img:String = randomImage(),
+    val img: String = randomImage(),
     val description: String
 )
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CardPreview() {
+    PersonCard(name = "gus", description = "lfsadkj")
+}
